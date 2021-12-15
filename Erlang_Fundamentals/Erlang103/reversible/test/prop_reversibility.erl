1b56a3496170999cee87d20159a78e5d620f39eb
James Lavin <james@jameslavin.com>
2021-12-15 09:53:34 -0500

    generate canned PropEr test file

---
+++ Erlang_Fundamentals/Erlang103/reversible/test/prop_reversibility.erl
@@ -0,0 +1,21 @@
+-module(prop_reversibility).
+-include_lib("proper/include/proper.hrl").
+
+%%%%%%%%%%%%%%%%%%
+%%% Properties %%%
+%%%%%%%%%%%%%%%%%%
+prop_test() ->
+    ?FORALL(Type, mytype(),
+        begin
+            boolean(Type)
+        end).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+%%%%%%%%%%%%%%%
+boolean(_) -> true.
+
+%%%%%%%%%%%%%%%%%%
+%%% Generators %%%
+%%%%%%%%%%%%%%%%%%
+mytype() -> term().
