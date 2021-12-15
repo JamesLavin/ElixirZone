568c845fe06a5ce9a987484da113e460a2e69b0f
James Lavin <james@jameslavin.com>
2021-12-15 10:09:11 -0500

    add prop_reversibility.erl test

--- Erlang_Fundamentals/Erlang103/reversible/test/prop_reversibility.erl
+++ Erlang_Fundamentals/Erlang103/reversible/test/prop_reversibility.erl
@@ -5,9 +5,9 @@
 %%% Properties %%%
 %%%%%%%%%%%%%%%%%%
 prop_test() ->
-    ?FORALL(Type, mytype(),
+    ?FORALL({Num, Denom}, {numerator(), denominator()},
         begin
-            boolean(Type)
+            is_number(Num / Denom)
         end).
 
 %%%%%%%%%%%%%%%
@@ -18,4 +18,12 @@ boolean(_) -> true.
 %%%%%%%%%%%%%%%%%%
 %%% Generators %%%
 %%%%%%%%%%%%%%%%%%
-mytype() -> term().
+numerator() -> integer().
+
+% This version won't catch the error because the inputs don't include 0
+% This is a reason why business logic bugs can be missed by ordinary example-based testing
+% denominator() -> oneof([range(1,10), range(11,100), range(101, 1000), range(1001,10000)]).
+
+% This version will (eventually) catch the error after hitting the test with more and
+% more inputs over time:
+denominator() -> oneof([range(1,10), range(11,100), range(101, 1000), range(1001,10000), integer()]).
